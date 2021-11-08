47972bd76f9385766a8003c8162109697d691216
Chris M <chris@adadevelopersacademy.org>
2021-11-07 20:05:01 -0800

    Initial commit

---
+++ src/fizzbuzz.js
@@ -0,0 +1,11 @@
+const fizzBuzz = (num) => {
+  if (num % 15 === 0) {
+    return "FizzBuzz";
+  } else if (num % 5 === 0) {
+    return "Buzz";
+  } else {
+    return num;
+  }
+};
+
+module.exports = { fizzBuzz };
---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,40 @@
+const { fizzBuzz } = require("/src/fizzbuzz");
+
+describe("fizzBuzz", () => {
+  test("returns Fizz on multiples of 3", () => {
+    // Arrange
+    const num = 9;
+
+    // Act
+    const result = fizzBuzz(num);
+
+    // Assert
+    expect(result).toEqual("Fizz");
+  });
+
+  test("returns Buzz on multiples of 5", () => {
+    expect(fizzBuzz(25)).toEqual("Buzz");
+  });
+
+  test("multiples of three and five return FizzBuzz", () => {
+    // Arrange
+    const num = 225;
+
+    // Act
+    const result = fizzBuzz(num);
+
+    // Assert
+    expect(result).toEqual("FizzBuzz");
+  });
+
+  test("non-multiples of three or five return the number itself", () => {
+    // Arrange
+    const num = 4;
+
+    // Act
+    const result = fizzBuzz(num);
+
+    // Assert
+    expect(result).toEqual(num);
+  });
+});
